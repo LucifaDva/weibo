47986223e9b30a3317a2504a255c4b11c5b4b34f
chuachua1080 <chuachua_1080@126.com>
2013-12-26 23:08:46 +0800

    weibo

---
+++ wei.py
@@ -0,0 +1,53 @@
+import requests
+import json
+class Weibo(object):
+    base_url = 'https://api.weibo.com/2/'
+    params = {}
+    headers = {}
+    
+    def __init__(self,access_token='',uid=''):
+        self.access_token  = access_token
+        self.uid = uid
+        
+    def _request(self,**kw):
+#        if 'headers' in kw:
+#            self.headers = kw['headers']
+#            del kw['headers']
+#        for keyword in ['content_length',
+#                        'content_md5',
+#                        'slice_md5',
+#                        'content_crc32']:
+#            try:
+#                kw[keyword.replace('_', '-')] = kw[keyword]
+#                del kw[keyword]
+#            except KeyError:
+#                continue
+                
+        self.params.update(access_token=self.access_token)
+        self.params.update(uid=self.uid)
+        self.params.update(kw)
+        self.url = ''.join([self.base_url,self.urlpath])
+    
+        if self._method == 'GET':
+            try:
+                r = requests.get(self.url,params=self.params,headers=self.headers)
+                return r.content
+            except Exception, e:
+                print e
+                return None
+        else:
+            return None
+            
+    def show(self,**kw):
+        self.urlpath = 'users/show.json'
+        self._method = 'GET'
+        result = self._request(**kw)
+        x = unicode(result,'utf8')
+        x.encode('utf8')
+        
+        return x
+    
+for i in range(1798847687,1798847700):
+    d = Weibo(access_token='2.0049mjxBCIwWIC4b97cb1107NWndaB',uid=str(i))
+    print 'uid:%s'%(str(i))
+    print d.show()    
